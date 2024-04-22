317e004a45564e76086efcb539089388028d3754
Aryamaniac <aryamansingh115@gmail.com>
2024-04-22 11:12:39 -0400

    Remove unused code files and update server configuration

--- web-server/app.js
+++ web-server/app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8082;
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+app.use(cors({origin: true, credentials: true}));
+
+//set routing for recipes
+const recipes = require('./routes/api/recipe');
+app.use('/api/recipe', recipes);
+
+//set routing for users
+const users = require('./routes/api/user');
+app.use('/api/user', users);
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
-const port = process.env.PORT || 8082;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const conn_str = 'mongodb+srv://as58810:BhkWrj6xZtDW8E7I@cluster0.wwmwleq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.set('strictQuery', false);
+mongoose.connect(conn_str).then(() => {
+    app.listen(port, () => console.log(`Server running on port ${port}`))
+    console.log('Connected to MongoDB')
+});
